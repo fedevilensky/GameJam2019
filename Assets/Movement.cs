55ffa1027371b418e0a05ce687808376c7672220
Federico Vilensky <fedevilensky@gmail.com>
2019-06-04 10:18:49 -0300

    code cloned from bitbucket repo

---
+++ Assets/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float facingAngle = 0;
+    float x = 0;
+    float z = 0;
+    Vector2 unitV2;
+
+    public GameObject cameraRef;
+    float distanceToCamera = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * -100;
+        z = Input.GetAxis("Horizontal") * Time.deltaTime * -500;
+
+        facingAngle += x;
+        unitV2 = new Vector2(Mathf.Cos(facingAngle * Mathf.Deg2Rad), Mathf.Sin(facingAngle * Mathf.Deg2Rad));
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.GetComponent<Rigidbody>().AddForce(new Vector3(unitV2.x, 0, unitV2.y) * x * 3);
+        cameraRef.transform.position = new Vector3(-unitV2.x * distanceToCamera, distanceToCamera, -unitV2.y * distanceToCamera) + this.transform.position;
+    }
+}
